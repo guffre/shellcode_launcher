719aa00d1e1af3bf206412364cff8b6b295b0173
Michael Guffre <mtguffre@gmail.com>
2019-03-10 17:28:38 -0400

    Update launcher.c

--- launcher.c
+++ launcher.c
@@ -8,35 +8,35 @@ int main(int argc, char **argv)
 	// argv == ebp + 0xC
 	__asm {
 		mov ebx, [ebp + 0xC]	// Save argv into ebx
-		cmp [ebp + 0x8], 2		// Check if there is a filename argument (argc == 2) 
+		cmp [ebp + 0x8], 2	// Check if there is a filename argument (argc == 2) 
 		jne $ + 0x5B
-		push 0x6272				// "rb"
-		push esp				// pointer to "rb"
-		push [ebx + 4]			// argv[1]
-		call fopen				// fopen(argv[1], "rb")
-		mov esi, eax			// esi = FILE
-		push 0x2				// SEEK_END
-		push 0					// 0
-		push esi				// FILE
-		call fseek				// fseek(FILE, 0, SEEK_END)
-		call ftell				// ftell(FILE)
+		push 0x6272		// "rb"
+		push esp		// pointer to "rb"
+		push [ebx + 4]		// argv[1]
+		call fopen		// fopen(argv[1], "rb")
+		mov esi, eax		// esi = FILE
+		push 0x2		// SEEK_END
+		push 0			// 0
+		push esi		// FILE
+		call fseek		// fseek(FILE, 0, SEEK_END)
+		call ftell		// ftell(FILE)
 		mov edi, eax
-		call rewind				// rewind(FILE)
-		push edi				// length of FILE
+		call rewind		// rewind(FILE)
+		push edi		// length of FILE
 		push 1
-		call calloc				// calloc(LENGTH, 1)
-		push esi				// FILE
+		call calloc		// calloc(LENGTH, 1)
+		push esi		// FILE
 		push 1
-		push edi				// length of FILE
-		push eax				// alloc'd mem
-		call fread				// fread(MEM, length, 1, FILE)
-		push ebx				// argv (trash pointer)
+		push edi		// length of FILE
+		push eax		// alloc'd mem
+		call fread		// fread(MEM, length, 1, FILE)
+		push ebx		// argv (trash pointer)
 		push 0x40
-		push edi				// length
-		push [esp + 0xC]		// MEM
-		call VirtualProtect		// VirtualProtect(MEM, length, 0x40, argv)
+		push edi		// length
+		push [esp + 0xC]	// MEM
+		call VirtualProtect	// VirtualProtect(MEM, length, 0x40, argv)
 		int 3
-		call [esp]				// Call shellcode
+		call [esp]		// Call shellcode
 		int 3
 	};
 }
